f2cc40fa6c1a4f041de63d8e8c90373b08802ce2
CoCo Chanel <veta583518@gmail.com>
2021-05-22 09:00:45 -0500

    added manifest.json file

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,10 +4,18 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   "./index.html",
-  "./css/style.css",
+  "./manifest.json",
+  "./css/styles.css",
+  "./icons/icon-72X72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
   "./js/idb.js",
   "./js/index.js",
-  "./icons/icon-72X72.png",
 ];
 
 self.addEventListener("install", function (e) {
